7c36d18e6824b9b7e089473a425f4116dbd6d320
Steffen Lund Andersen <SteffenLundAndersen@Gmail.com>
2020-07-01 12:46:01 +0200

    Version 1.1.3

--- QuickPayExample/QuickPayExample/ShopViewController.swift
+++ QuickPayExample/QuickPayExample/ShopViewController.swift
@@ -67,6 +67,15 @@ class ShopViewController: UIViewController {
         
         if let paymentOption = paymentView.getSelectedPaymentOption() {
             switch paymentOption {
+//            case .mobilepay:
+//                if !QuickPay.isMobilePayAvailableOnDevice() {
+//                    displayOkAlert(title: "MobilePay Error", message: "MobilePay is not installed on this device.")
+//                }
+//                else {
+//                    handleMobilePay()
+//                }
+//                break
+                
             case .paymentcard:
                 handlePaymentWindow()
                 break
@@ -74,6 +83,10 @@ class ShopViewController: UIViewController {
             case .applepay:
                 handleApplePay()
                 break
+                
+//            case .vipps:
+//                handleVipps()
+//                break
             }
         }
     }
@@ -189,6 +202,57 @@ class ShopViewController: UIViewController {
         }
     }
     
+}
+
+
+// MARK: - MobilePay
+/**
+ Example code to demonstrate the use of MobilePay
+ 
+ The steps needed to use MobilePay is
+ 1) Create a payment
+ 2) Create a payment session
+ 3) Authorize the payment through MobilePay
+ 4) Validate that the authoprization went well
+ */
+extension ShopViewController {
+    
+    func handleMobilePay() {
+        // Show a progress indicator
+        showSpinner(onView: self.view)
+        
+        // Step 1) Create a payment
+        QPCreatePaymentRequest(parameters: createPaymentParametersFromBasket()).sendRequest(success: { (payment) in
+            
+            // Step 2) Create a payment session
+            // You need to specify a return url that MobilePay can query to get back to your app
+            let mpp = MobilePayParameters(returnUrl: "quickpayexampleshop://", language: "dk", shopLogoUrl: "https://quickpay.net/images/payment-methods/payment-methods.png")
+            let createSessionRequestParameters = QPCreatePaymentSessionParameters(amount: Int(self.totalBasketValue() * 100), mobilePay: mpp)
+            QPCreatePaymentSessionRequest(id: payment.id, parameters: createSessionRequestParameters).sendRequest(success: { (payment) in
+                
+                // Step 3) Authorize the payment through MobilePay
+                // This step will query the MobilePay app and the completion handler will be invoked when your app is opened up again
+                QuickPay.authorizeWithMobilePay(payment: payment, completion: { (payment) in
+                    
+                    // Step 4) Validate that the authoprization went well
+                    QPGetPaymentRequest(id: payment.id).sendRequest(success: { (payment) in
+                        self.removeSpinner()
+                        
+                        if payment.accepted {
+                            self.displayOkAlert(title: "Payment Accepted", message: "The payment was accepted and the acquirer is \(payment.acquirer ?? "unknown")")
+                            // Congratulations, you have successfully authorized the payment.
+                            // You can now capture the payment when you have shipped the items.
+                        }
+                        else {
+                            self.displayOkAlert(title: "Payment Not Accepted", message: "The payment was not accepted")
+                        }
+                    }, failure: self.handleQuickPayNetworkErrors)
+                }, failure: {
+                    self.displayOkAlert(title: "MobilePay Failed", message: "Could not authorize with MobilePay")
+                })
+            }, failure: self.handleQuickPayNetworkErrors)
+        }, failure: self.handleQuickPayNetworkErrors)
+    }
 }
 
 
@@ -314,6 +378,16 @@ extension ShopViewController: PKPaymentAuthorizationViewControllerDelegate {
     
 }
 
+// MARK: - Vipps
+extension ShopViewController {
+    
+    func handleVipps() {
+        //TODO: Implement
+        
+        print("VIPPS!!")
+    }
+    
+}
 
 // MARK: - Payment Window
 /**
@@ -342,13 +416,10 @@ extension ShopViewController {
                 // Step 3) Open the payment URL in a WebView
                 QuickPay.openPaymentLink(paymentUrl: paymentLink.url, onCancel: {
                     self.removeSpinner()
-                    
                     self.displayOkAlert(title: "Payment Cancelled", message: "The payment was cancelled")
                 }, onResponse: { (success) in
                     // Step 4) Validate that the authoprization went well
                     QPGetPaymentRequest(id: payment.id).sendRequest(success: { (payment) in
-                        self.removeSpinner()
-                        
                         if payment.accepted {
                             self.displayOkAlert(title: "Payment Accepted", message: "The payment was accepted and the acquirer is \(payment.acquirer ?? "unknown")")
                             // Congratulations, you have successfully authorized the payment.
--- README.md
+++ README.md
@@ -17,11 +17,6 @@ target '<YOUR_PROJECT_NAME>' do
 end
 ```
 
-### Swift Versions
-
-QuickPaySDK version 1.1.2 Swift 5.2  
-QuickPaySDK version 1.1.0 and 1.1.1 is build with Swift 5.1  
-QuickPaySDK version < 1.1.0 is build with Swift 5.0
 
 ### Fat library
 
@@ -71,10 +66,13 @@ done
 
 ### API key and permissions
 
-In order for the SDK to communicate with QuickPay, you will need an API key. You can create one by logging in to your QuickPay account and navigate to Settings -> Users. The API key you use with the SDK needs an additional permission in order to work with Apple Pay. Select the user to which the API key belongs and add the following permission.
+In order for the SDK to communicate with QuickPay, you will need an API key. You can create one by logging in to your QuickPay account and navigate to Settings -> Users. The API key you use with the SDK needs some additional permissions in order to work with Apple Pay, MobilePay and Vipps. Select the user to which the API key belongs and add the following permissions.
 
 ```html
-GET  /acquirers/clearhaus
+GET  /acquirers/clearhaus   (Apple Pay)
+GET  /acquirers/mobilepay   (MobilePay)
+GET  /acquirers/vipps       (Vipps)
+POST /payments/:id/session  (MobilePay + Vipps)
 ```
 
 
@@ -163,6 +161,58 @@ QPGetPaymentRequest(id: payment.id).sendRequest(success: { (payment) in
 ```
 
 If you want more control of the payment window and want to handle the navigation yourself, you can create a `QPPaymentWindowController` and pass the payment url in its initializer. Next, you set a `QPPaymentWindowControllerDelegate` to the payment window controller and you can now have full control over how to present the payment view. Through the delegate, you can also create a custom loading view that will be shown while the payment window is getting loaded and rendered.
+
+
+### MobilePay
+
+QuickPay SDK supports MobilePay natively so you can create a great app experience. To query the MobilePay App you need to make some changes to your project settings and implement the payment as shown later in this guide.
+
+It is recommended that you check if the users have the MobilePay App installed and only show the payment option if it is available. The QuickPay class can help you with this.
+
+```swift
+QuickPay.isMobilePayAvailable()
+```
+
+
+#### URL Schemes for MobilePay
+
+To query the MobilePay App you need to whitelist the `mobilepayonline` URL scheme to the `LSApplicationQueriesSchemes` array in your `Info.plist`. With this done your application can now query the MobilePay App with the needed information for MobilePay to handle the authorization of a payment.
+
+You also need to specify a custom URL scheme for the MobilePay App to query back to when it is done doing its job. This is done in the `URL types` array in your `Info.plist`.
+
+You can read more about the URL schemes on [https://developers.apple.com](https://developer.apple.com/documentation/uikit/core_app/allowing_apps_and_websites_to_link_to_your_content/defining_a_custom_url_scheme_for_your_app)
+
+
+#### Payment flow for MobilePay
+
+First, you need to create a payment just like with the payment window, but after that, the flow is different since we do not have the payment window to handle a lot of tasks for us.
+
+When you have created your payment you need to start a MobilePay payment session. Create a `MobilePayParameters` object and specify the custom URL scheme that you created earlier. You can also specify the language of MobilePay and add a URL to a logo you want it to be displayed. Add this information to a `QPCreatePaymenSessionParameters` along with the amount of money you want to authorize. Finally, put everything together in a `QPCreatePaymenSessionRequest` and send the request.
+
+```swift
+let mobilePayParameters = MobilePayParameters(returnUrl: "quickpayexampleshop://", language: "dk", shopLogoUrl: "https://SomeUrl/SomeImage.png")
+let sessionParameters = QPCreatePaymenSessionParameters(amount: 100, mobilePay: mpp)
+
+let request = QPCreatePaymenSessionRequest(id: payment.id, parameters: sessionParameters)
+
+request.sendRequest(success: { (payment) in
+    // Handle the payment
+}, failure: { (data, response, error) in
+    // Handle the failure
+})
+```
+
+With the payment containing a session id, we can now query MobilePay to authorize the payment.
+
+```swift
+QuickPay.authorizeWithMobilePay(payment: payment, completion: { (payment) in {
+    // Handle the payment
+}, failure: {
+    // Handle the failure
+}
+```
+
+In the completion handler, we need to check the status of the payment. This is done in the same way as with the payment window. Create a `QPGetPaymentRequest` with the payment id and check if the updated payment is accepted.
 
 
 ### Apple Pay
